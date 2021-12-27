636eb7167e5a49af4e38cc37e72b66435c472dc0
Ravin <ravin.desilva@stud.fh-campuswien.ac.at>
2021-12-27 13:46:30 +0100

    beginning of program

--- src/main/java/at/uni/prog/Main.java
+++ src/main/java/at/uni/prog/Main.java
@@ -5,17 +5,84 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        launch(args);
+public class Main {
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("C:/Users/Ravin/Desktop/HangmanList.txt"));
+        Scanner guessing= new Scanner(System.in);
+
+        List<String> words = new ArrayList<>();
+
+
+        while (scanner.hasNext()){
+            words.add(scanner.nextLine());
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Hello World!");
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Random rand = new Random();
+
+        String word = words.get(rand.nextInt(words.size()));
+
+
+        System.out.println(word);
+
+
+
+        List<Character> playerGuesses = new ArrayList<>();
+        printWordState(word, playerGuesses);
+
+        System.out.println("Enter a letter:");
+        String guessingLetter = guessing.nextLine();
+        playerGuesses.add(guessingLetter.charAt(0));
+
+        printWordState(word, playerGuesses);
+
+        while (true) {
+            getPlayerGuess(guessing, word, playerGuesses);
+            if(printWordState(word, playerGuesses)){
+                System.out.println("You win!");
+                break;
+            }
+
+        }
+
+        }
+
+        private static void getPlayerGuess(Scanner guessing, String word,List<Character> playerGuesses ){
+            System.out.println("Enter a letter:");
+            String guessingLetter = guessing.nextLine();
+            playerGuesses.add(guessingLetter.charAt(0));
+
+
+        }
+
+    private static boolean printWordState(String word, List<Character> playerGuesses){
+        int countGuess = 0;
+        int falseGuess = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (playerGuesses.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+                countGuess++;
+            } else {
+                System.out.print("-");
+                falseGuess++;
+            }
+        }
+        System.out.println();
+
+    return (word.length()==countGuess);
     }
+
+
+
+
 }

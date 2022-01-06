b7b29d02854c1c45466e8a620f93564a6728a897
naviddariya <n.dariya@ementalist.ai>
2022-01-06 16:14:07 +0100

    Removed hardcoded file path for getting list Added the ability to identify both lowercase and uppercase

--- src/main/java/at/uni/prog/Main.java
+++ src/main/java/at/uni/prog/Main.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -17,17 +18,16 @@ public class Main {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println();
-
-        Scanner scanner = new Scanner(new File("C:/Users/Ravin/Desktop/HangmanList.txt"));
+        File file = new File(
+                Main.class.getClassLoader().getResource("list.txt").getFile());
+        Scanner scanner = new Scanner(file);
         Scanner guessing= new Scanner(System.in);
 
         List<String> words = new ArrayList<>();
 
-
-        while (scanner.hasNext()){
+        while (scanner.hasNext()) {
             words.add(scanner.nextLine());
-    }
+        }
 
         Random rand = new Random();
 
@@ -53,24 +53,33 @@ public class Main {
                 System.out.println("You win!");
                 break;
             }
-
         }
 
         }
 
-        private static void getPlayerGuess(Scanner guessing, String word,List<Character> playerGuesses ){
-            System.out.println("Enter a letter:");
-            String guessingLetter = guessing.nextLine();
-            playerGuesses.add(guessingLetter.charAt(0));
-
-
-        }
+    /**
+     *
+     * @param guessing
+     * @param word
+     * @param playerGuesses
+     */
+    private static void getPlayerGuess(Scanner guessing, String word,List<Character> playerGuesses ) {
+        System.out.println("Enter a letter:");
+        String guessingLetter = guessing.nextLine();
+        playerGuesses.add(guessingLetter.charAt(0));
+    }
 
     private static boolean printWordState(String word, List<Character> playerGuesses){
+        // In order to ignore case sensitive differences
+        List<Character> lowercasePlayerGuesses = new ArrayList<>();
+        for(Character c: playerGuesses) {
+            lowercasePlayerGuesses.add(Character.toLowerCase(c));
+        }
         int countGuess = 0;
         int falseGuess = 0;
         for (int i = 0; i < word.length(); i++) {
-            if (playerGuesses.contains(word.charAt(i))) {
+            char lowerCaseExpectedChar = Character.toLowerCase(word.charAt(i));
+            if (lowercasePlayerGuesses.contains(lowerCaseExpectedChar)) {
                 System.out.print(word.charAt(i));
                 countGuess++;
             } else {
